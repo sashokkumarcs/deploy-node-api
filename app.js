de28ac456524d9272a6cddbf4163d184feece3ae
Ashok kumar S <sashokcs@gmail.com>
2024-10-31 21:21:03 +0000

    Simple app.js

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
-require('dotenv').config()
 
 const port = process.env.PORT || 4200
 
-app.use(express.static('public'))
-
-app.use(cors())
-// Set EJS as the view engine for express
-app.set('view engine', 'ejs')
-app.set('views', './views')
-
-
 app.get('/', (req, res)=>{
-    res.render('index' , {title: "EJS Demo App"})
+    res.send("This is home page")
 })
 
 app.get('/about', (req, res)=>{
-    res.render('about', {title: "EJS Demo App"})
+    res.send('This is about page')
 })
 
 app.listen(port, ()=> console.log(`Server running at http://localhost:${port}`))
